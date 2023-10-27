525c78b5db6e2fe9ce5ee890c446fe1795686389
teq-daihoang <daihoang@teqnological.asia>
2023-10-27 09:04:54 +0700

    island architecture

--- src/components/Slick/index.tsx
+++ src/components/Slick/index.tsx
@@ -1,39 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import type { IMUAPI } from "../../types";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 interface Props {
   data: IMUAPI[];
 }
 const Slick = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+  const [hydrate, setHydrate] = useState(false);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  useEffect(() => {
+    setHydrate(true);
+  }, []);
+  if (!hydrate) {
+    return (
+      <div>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </div>
+    );
+  }
   return (
-    <Slider {...settings}>
     <div>
-      <h3>1</h3>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
-    <div>
-      <h3>2</h3>
-    </div>
-    <div>
-      <h3>3</h3>
-    </div>
-    <div>
-      <h3>4</h3>
-    </div>
-    <div>
-      <h3>5</h3>
-    </div>
-    <div>
-      <h3>6</h3>
-    </div>
-  </Slider>
-  )
-}
+  );
+};
 
-export default Slick
+export default Slick;
