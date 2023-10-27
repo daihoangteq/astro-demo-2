fea661b0142b178ca2cc60149b501cd9941f2b7b
teq-daihoang <daihoang@teqnological.asia>
2023-10-27 09:24:05 +0700

    update island architecture

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
-interface Props {
-  title: string;
-}
-const Button: React.FC<Props> = ({ title }) => {
+const Button = () => {
   const [isHydrate, setIsHydrate] = useState(false);
   useEffect(() => {
     setIsHydrate(true);
   }, []);
   return (
-    <div className="w-fit h-fit p-2 rounded-sm" style={{ backgroundColor: isHydrate ? "green" : "red" }}>{title}</div>
+    <div className="w-fit h-fit p-2 rounded-sm" style={{ backgroundColor: isHydrate ? "green" : "red" }}>{isHydrate? "Hydrate" : "Non"}</div>
   );
 };
 
--- src/components/buttons/ButtonNonHydrate.tsx
+++ src/components/buttons/ButtonNonHydrate.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
-interface Props {
-  title: string;
-}
-const ButtonNonHydrate: React.FC<Props> = ({ title }) => {
+const Button = () => {
   const [isHydrate, setIsHydrate] = useState(false);
   useEffect(() => {
     setIsHydrate(true);
   }, []);
   return (
-    <div className="w-fit h-fit p-2 rounded-sm" style={{ backgroundColor: isHydrate ? "green" : "red" }}>{title}</div>
+    <div className="w-fit h-fit p-2 rounded-sm" style={{ backgroundColor: isHydrate ? "green" : "red" }}>{isHydrate? "Hydrate" : "Non"}</div>
   );
 };
 
-export default ButtonNonHydrate;
+export default Button;
